84b95d1b001bcea2d0bbeb6c6822ccb1da059e5f
Ved Pahune <114849889+vednp@users.noreply.github.com>
2023-03-10 20:30:38 +0530

    Blog Website Using Database CRUD Operations
    Blog Website Using Database CRUD Operations

---
+++ Blog-Website-db/database.js
@@ -0,0 +1,18 @@
+const mongodb = require('mongodb')
+
+const MongoClient = mongodb.MongoClient;
+
+let database;
+async function connect(){
+    const client = await MongoClient.connect('mongodb://127.0.0.1:27017');
+    database = client.db('blog')
+}
+
+function getDb(){
+    return database
+}
+
+module.exports = {
+    connect:connect,
+    getDb:getDb
+}
